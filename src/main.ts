5ff7511922fc9bfa9e68405dcec52c17148501ce
Damian Adams <damian.adams@rewardstyle.com>
2024-12-12 17:33:19 -0600

    Set locale cookie on app bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import Cookies from 'universal-cookie'
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+const cookies = new Cookies(null, { path: '/' })
+cookies.set('locale', cookies.get('locale') || 'en')
 
 const app = createApp(App)
 
